dce9ea304036d07615004845811f882827f6d435
RomainSav <romain.sv@icloud.com>
2023-06-02 18:39:33 +0200

    Update styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,5 +5,32 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./lib/**/*.{js,ts,jsx,tsx}"
   ],
-  theme: {}
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1480px"
+      }
+    },
+
+    colors: {
+      primary: "#EE1D23",
+      white: {
+        DEFAULT: "#FFFFFF",
+        1: "#F5F5F5",
+        2: "#EEEDED"
+      },
+
+      gray: {
+        1: "#090909",
+        2: "#1B1B1B",
+        3: "#323232"
+      }
+    }
+  }
 } satisfies Config;
