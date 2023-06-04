e6732097a452099a1da63c2e53e77942d1b97b8f
RomainSav <romain.sv@icloud.com>
2023-06-04 11:40:28 +0200

    Update page.tsx

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,11 +1,11 @@
-import { getMovies } from "@lib/configs/movies";
+import { movies } from "@lib/configs/movies";
 import type { ReactElement } from "react";
 import { MovieCard } from "./movie-card";
 
 const Page = (): ReactElement => {
   return (
     <div className="container grid grid-cols-5 mt-16 gap-10">
-      {getMovies().map((movie) => (
+      {movies.map((movie) => (
         <MovieCard key={movie.id} movie={movie} />
       ))}
     </div>
