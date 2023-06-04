43cfb060da46b60bad10f7b23589844d41f6ec11
RomainSav <romain.sv@icloud.com>
2023-06-04 12:35:43 +0200

    Update particles.tsx

--- lib/components/particles/particles.tsx
+++ lib/components/particles/particles.tsx
@@ -5,13 +5,15 @@ import type { Engine } from "tsparticles-engine";
 import { Particles as ReactParticles } from "react-particles";
 import { loadFull } from "tsparticles";
 import { particlesConfig } from "./particles.config";
+import { useIsDomLoaded } from "@lib/hooks/is-dom-loaded";
 
 export const Particles = (): ReactElement | null => {
-  const particlesInit = async(engine: Engine): Promise<void> => {
+  const isDomLoaded = useIsDomLoaded();
+  if (!isDomLoaded) return null;
+
+  const particlesInit = async (engine: Engine): Promise<void> => {
     await loadFull(engine);
   };
-
-  if (typeof window === "undefined") return null;
 
   return <ReactParticles id="tsparticles" options={particlesConfig} init={particlesInit} />;
 };
